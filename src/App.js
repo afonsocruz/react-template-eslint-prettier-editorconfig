20c389b3ca9029eaabe8fbd3c611012fe71eb776
Afonso <afonsocruz.rj@gmail.com>
2021-02-12 22:03:13 -0300

    all set

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Simple eslint, prettier and editorconfig setup</h1>
+      <h3>Airbnb styleguide / JavaScript / React / No TypeScript</h3>
+      <p>Everything working</p>
+      <p>All you need to change is your vscode settings:</p>
+      <p>
+        &quot;editor.formatOnSave&quot;: true,
+        <br />
+        &quot;editor.codeActionsOnSave&quot;: {'{'}
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&quot;source.fixAll.eslint&quot;: true <br />
+        {'}'}
+      </p>
+    </>
   );
 }
 
